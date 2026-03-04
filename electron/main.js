6f3cffab89ed28e2d8575f1e9635b016f2dc2faf
magenta550-ui <magenta550@gmail.com>
2026-03-04 17:18:24 +0900

    Update main.js

--- electron/main.js
+++ electron/main.js
@@ -30,6 +30,7 @@ function createWindow() {
     win.loadFile(path.join(__dirname, '../dist/index.html'))
   }
 }
+app.userAgentFallback = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
 
 app.whenReady().then(createWindow)
 
