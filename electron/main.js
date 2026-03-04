b7e0cd822da02efc34285aa229e450c7ab1dfb0e
magenta550-ui <magenta550@gmail.com>
2026-03-04 16:54:19 +0900

    Create main.js

---
+++ electron/main.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow, shell } = require('electron')
+const path = require('path')
+
+const isDev = process.env.NODE_ENV === 'development'
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 860,
+    minWidth: 900,
+    minHeight: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+    icon: path.join(__dirname, '../public/icon.ico'),
+    title: 'YouTube Channel Scanner',
+    backgroundColor: '#1a1a2e',
+    autoHideMenuBar: true,
+  })
+
+  win.webContents.setWindowOpenHandler(({ url }) => {
+    shell.openExternal(url)
+    return { action: 'deny' }
+  })
+
+  if (isDev) {
+    win.loadURL('http://localhost:5173')
+  } else {
+    win.loadFile(path.join(__dirname, '../dist/index.html'))
+  }
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
